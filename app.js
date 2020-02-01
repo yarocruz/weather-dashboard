4ac6dc947f5a8c12470a07a6ecb22535bbec8cb6
yarocruz <yarocruz@gmail.com>
2020-02-01 09:13:16 -0500

    added input and list group

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Open Weather API Key 23cfcf98d26d55db92e1f501489be10f
 let APIkey = '23cfcf98d26d55db92e1f501489be10f'
 let city = 'Orlando';
-let queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`;
+let queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city},US&appid=${APIkey}`;
 
 $.ajax({
     url: queryURL,
