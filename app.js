59fb6e51aa2a8694815d8aaf535c91f0e1af06f7
yarocruz <yarocruz@gmail.com>
2020-02-01 20:53:14 -0500

    added function that gets city data

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 // Open Weather API Key 23cfcf98d26d55db92e1f501489be10f
 let APIkey = '23cfcf98d26d55db92e1f501489be10f'
-let city = 'Orlando';
-let queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city},US&appid=${APIkey}`;
 
-$.ajax({
-    url: queryURL,
-    method: 'get'
-}).then((res) => {
-    console.log(res);
-})
+
+
+$('.input-group-text').click(getCityWeather);
+
+function getCityWeather() {
+    let city = $('.input-search').val();
+    let queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIkey}`;
+
+    console.log(city);
+
+    $.ajax({
+        url: queryURL,
+        method: 'get'
+    }).then((res) => {
+        console.log(res);
+        $('.city-heading').text(res.name);
+        $('.temp').text(res.main.temp);
+        $('.humidity').text(res.main.humidity);
+        $('.wind-speed').text(res.wind.speed);
+        //$('.uv-index').text(res.main.temp);
+        $('.input-search').val('')
+    })
+}
+
+
+
